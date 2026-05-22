7cd11048ad7c6de4eb4149c3d8816de8b1f9a9aa
samulx_termux <cubasamuel852@gma*l.com>
2026-05-21 21:35:55 -0400

    Switch audio query plugin fork

--- lib/pages/music_list.dart
+++ lib/pages/music_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:on_audio_query/on_audio_query.dart';
+import 'package:on_audio_query_forked/on_audio_query.dart';
 
 class MusicList extends StatefulWidget {
   const MusicList({super.key});
--- lib/pages/music_player.dart
+++ lib/pages/music_player.dart
@@ -2,7 +2,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:on_audio_query/on_audio_query.dart';
+import 'package:on_audio_query_forked/on_audio_query.dart';
 
 import 'package:palette_generator/palette_generator.dart';
 
